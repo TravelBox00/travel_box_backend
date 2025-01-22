716c0174f6e4391626bb2d1dfa3d3c55846960df
dania0x4C <ljm31881004@gmail.com>
2025-01-23 07:10:15 +0900

    [refactor] decode token to userTag

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 dotenv.config();
 
@@ -46,3 +46,14 @@ export const authenticateToken = async (req: Request, res: Response, next: NextF
 사용 방법
 app.get('/users', authenticateToken, (req, res) => {});
 */
+
+export const decodeTokenUserTag = (token: string) => {
+    try {
+        const decoded = jwt.decode(token);
+        const userTag: string | undefined = (decoded as JwtPayload).userTag;
+        return userTag;
+    } catch (error) {
+        console.error("Failed to decode token:", error);
+        return null;
+    }
+};
