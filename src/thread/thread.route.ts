6b9eccacf9d7d8de10c109e203e90586ba580bbf
Gjuni <kmj06178@gmail.com>
2025-02-15 12:09:49 +0900

    post categrot

--- src/thread/thread.route.ts
+++ src/thread/thread.route.ts
@@ -251,8 +251,7 @@ router.get('/scrap/info', getScrappedThreads);
  *                     "postContent": "스웨거 테스트 용 내용",
  *                     "postRegionCode": "서울 강남",
  *                     "clothId": 1,
- *                     "songName": "Inside Out",
- *                     "chooseNum": 0
+ *                     "songName": "Inside Out"
  *                   }
  *               files:
  *                 type: array
